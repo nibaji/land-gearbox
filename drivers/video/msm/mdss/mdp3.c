arter97/caf_msm-5.4

msm: mdss: free splash memory for MSM8909w after splash done
Splash memory free calls was failing for MSM8909w because
splash memory length was not populated properly after device tree parsing.
Use cont-splash-memory property to read splash mem length when
memblock-reserve is not defined.

Change-Id: I1a79c1a6eeb2da6f8238bcc1f82ccad0c6891569
Signed-off-by: Sachin Bhayare <sachin.bhayare@codeaurora.org>

drivers/video/msm/mdss/mdp3.c
@@ -2036,9 +2036,8 @@ __ref int mdp3_parse_dt_splash(struct msm_fb_data_type *mfd)
 	prop = of_find_property(pdev->dev.of_node, "qcom,memblock-reserve",
 				&len);
 	if (!prop) {
-		pr_err("error reading memblock reserve settings for fb\n");
-		rc = -EINVAL;
-		goto error;
+		pr_debug("Read memblock reserve settings for fb failed\n");
+		pr_info("Read cont-splash-memory settings\n");
 	}
 
 	if (len) {

nibaji/land-gearbox

drivers/video/msm/mdss/mdp3.c
@@ -2035,9 +2035,8 @@ __ref int mdp3_parse_dt_splash(struct msm_fb_data_type *mfd)
 	prop = of_find_property(pdev->dev.of_node, "qcom,memblock-reserve",
 				&len);
 	if (!prop) {
-		pr_err("error reading memblock reserve settings for fb\n");
-		rc = -EINVAL;
-		goto error;
+		pr_debug("Read memblock reserve settings for fb failed\n");
+		pr_debug("Read cont-splash-memory settings\n");
 	}
 
 	if (len) {